0ed95569142a9612df3a86fb59bee03421a27e4b
AimraneHD <96395230+AimraneHD@users.noreply.github.com>
2026-06-20 20:18:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -16,10 +16,10 @@ Hello world\n
 My name is Aimrane Haddou, I'm a 2nd year student, and will be a 3rd year one in a few months\n
 Let's see what happens\n
 $$
-\\boxed{\\text{Here's a random maxwell equation} \\implies \\boxed{ \\vec{rot}(\\vec{E})=-\\frac{ \\partial\\vec{B} }{ \\partial t }}}\n
+\\text{Here's a random maxwell equation} \\implies \\boxed{ \\vec{rot}(\\vec{E})=-\\frac{ \\partial\\vec{B} }{ \\partial t }}\n
 $$
 fixed it, kind of\n
 
-$$ Experiment\\ number\\ two\\ \\text{(i love latex's italic computer modern font instead of the normal one)} $$
+$$ Experiment\\ number\\ two\\ \\text{(i love latex's italic computer modern font instead of the normal one)}\n $$
 
 """
