2d2e1d4b544de8a4b431e6cf0a10afbbdf61563f
AimraneHD <96395230+AimraneHD@users.noreply.github.com>
2026-06-20 17:39:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -77,20 +77,20 @@ st.write(pd.DataFrame({'A': [1, 2], 'B': [3, 4]}))
 st.write("hmm")
 
 st.write("i noticed this text format almost looks like markdown, i wonder if i can...")
-st.write("""$$\n
+st.write(\"\"\"$$\n
 \\begin{equation}\n
 e=-\\frac{d\\phi}{dt}=\\oint(\\vec{E}+\\vec{v}\\times\\vec{B}).\\vec{dl}\n
 \\end{equation}\n
-$$""")
+$$\"\"\")
 st.write("i dont want it to put a number next to an equation though")
 st.write("one more thing, what if i...")
-st.write("""
+st.write(\"\"\"
 | full name | Massar code | Ensam code | Current grade |
 |-----------|-------------|------------|---------------|
 | Aimrane Haddou | B136070002 | M24401 | 2nd year |
-""")
+\"\"\")
 st.write("wait hold on..., i can just write everything in one whole string like i would in a markdown file without having to write \"st.write\" over and over again")
-st.write("""
+st.write(\"\"\"
 Hello, my name is Aimrane Haddou, I am currently a second year student, well..., was a second year student, but not officially a 3rd year one yet, summer just started\n
 Here is my totally real ensam code: $M24401$\n
 Here is my totally real Massar code as well: $B136070002$\n
@@ -106,7 +106,7 @@ $$
 e=-\\frac{d\\phi}{dt}=\\oint(\\vec{E}+\\vec{v}\\times\\vec{B}).\\vec{dl}\n
 \\end{equation}
 $$
-""")
+\"\"\")
 st.write({"Name" : "Gemini", "Role": "AI"})
 ```
 """)
