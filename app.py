d0ce1dc5ee0898d2458f9462c7199923dcec84fd
AimraneHD <96395230+AimraneHD@users.noreply.github.com>
2026-06-20 20:17:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,5 @@
 import streamlit as st
-from streamlit.components.v1 import html
-
-# --- HIDE NATIVE STREAMLIT BRANDING (Footer, Menu, Header) ---
-hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_st_style, unsafe_allow_html=True)
-
-# --- DESTROY THE STREAMLIT.IO CLOUD WATERMARK ---
-html('''
-<script>
-window.top.document.querySelectorAll(`[href*="streamlit.io"]`).forEach(e => e.setAttribute("style", "display: none;"));
-</script>
-''', width=0, height=0)
+import watermark_remover
 
 # --- Your normal app code continues down here ---
 st.title("My Portal")
