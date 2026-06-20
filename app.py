91074360d1d8e512d7ce3317ceadf21b3f5ca912
AimraneHD <96395230+AimraneHD@users.noreply.github.com>
2026-06-20 17:02:03 +0100

    Enhance app.py with more heading levels
    Added additional headings to the Streamlit app.

--- app.py
+++ app.py
@@ -4,3 +4,6 @@ import pandas as pd
 st.write("Hello world")
 st.write("## This is a heading")
 st.write("### wonder what happens here")
+st.write("#### this is interesting")
+st.write("##### is this where it stops?")
+st.write("###### lets see")
