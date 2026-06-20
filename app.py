3bad5345c3660bba7ebf13e266b81707695b8646
AimraneHD <96395230+AimraneHD@users.noreply.github.com>
2026-06-20 17:20:21 +0100

    Refactor app.py to restore original content

--- app.py
+++ app.py
@@ -20,8 +20,8 @@ st.write(pd.DataFrame({'A': [1, 2], 'B': [3, 4]}))
 st.write("hmm")
 
 st.write("i noticed this text format almost looks like markdown, i wonder if i can...")
-st.write("""$
-\\begin{equation}
-  e=-\\frac{d\\phi}{dt}=\\oint(\\vec{E}+\\vec{v}\\times\\vec{B}).\\vec{dl}$
-\\end{equation}
-$""")
+st.write("""\n
+\\begin{equation}\n
+  e=-\\frac{d\\phi}{dt}=\\oint(\\vec{E}+\\vec{v}\\times\\vec{B}).\\vec{dl}\n
+\\end{equation}\n
+""")
