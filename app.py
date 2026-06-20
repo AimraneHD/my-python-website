b83dd87d7e09d6c325babda44a4303c603053140
AimraneHD <96395230+AimraneHD@users.noreply.github.com>
2026-06-20 18:25:28 +0100

    Refactor app.py to add missing content

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ Hello world\n
 My name is Aimrane Haddou, I'm a 2nd year student, and will be a 3rd year one in a few months\n
 Let's see what happens\n
 $$
-\\boxed{\\text{Here's a random maxwell equation} \\implies \\boxed{ \\vec{rot}(\\vec{E})=-\\frac{ \\partial\\vec{B} }{ \\partialt }}\n
+\\boxed{\\text{Here's a random maxwell equation} \\boxed{ \\vec{rot}(\\vec{E})=-\\frac{ \\partial\\vec{B} }{ \\partialt }}\n
 $$
+nothing?
 """
