115e9215886fc78583b7270ac951d4a5c76c10f7
AimraneHD <96395230+AimraneHD@users.noreply.github.com>
2026-06-20 18:26:26 +0100

    Modify app.py content and add equations
    Updated the content in app.py to include additional text and equations.

--- app.py
+++ app.py
@@ -6,7 +6,10 @@ Hello world\n
 My name is Aimrane Haddou, I'm a 2nd year student, and will be a 3rd year one in a few months\n
 Let's see what happens\n
 $$
-\\boxed{\\text{Here's a random maxwell equation} \\boxed{ \\vec{rot}(\\vec{E})=-\\frac{ \\partial\\vec{B} }{ \\partialt }}\n
+\\boxed{\\text{Here's a random maxwell equation} \\boxed{ \\vec{rot}(\\vec{E})=-\\frac{ \\partial\\vec{B} }{ \\partialt }}}\n
 $$
-nothing?
+$$
+\\text{Here's a random maxwell equation} \\boxed{ \\vec{rot}(\\vec{E})=-\\frac{ \\partial\\vec{B} }{ \\partialt }}\n
+$$
+nothing. still nothing.
 """
