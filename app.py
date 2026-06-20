b7432c2995eb7ed3d2d881e2b1ed77bfc9eab81e
AimraneHD <96395230+AimraneHD@users.noreply.github.com>
2026-06-20 18:26:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,7 @@ Hello world\n
 My name is Aimrane Haddou, I'm a 2nd year student, and will be a 3rd year one in a few months\n
 Let's see what happens\n
 $$
-\\boxed{\\text{Here's a random maxwell equation} \\boxed{ \\vec{rot}(\\vec{E})=-\\frac{ \\partial\\vec{B} }{ \\partialt }}}\n
+\\boxed{\\text{Here's a random maxwell equation} \\boxed{ \\vec{rot}(\\vec{E})=-\\frac{ \\partial\\vec{B} }{ \\partial t }}}\n
 $$
-$$
-\\text{Here's a random maxwell equation} \\boxed{ \\vec{rot}(\\vec{E})=-\\frac{ \\partial\\vec{B} }{ \\partialt }}\n
-$$
-nothing. still nothing.
+fixed it, kind of\n
 """
