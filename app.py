4ed569c58490b9de791f774d1f25c896e6646c3d
AimraneHD <96395230+AimraneHD@users.noreply.github.com>
2026-06-20 17:03:44 +0100

    Enhance app.py with dynamic heading generation
    Added a loop to generate headings dynamically.

--- app.py
+++ app.py
@@ -7,3 +7,9 @@ st.write("### wonder what happens here")
 st.write("#### this is interesting")
 st.write("##### is this where it stops?")
 st.write("###### lets see")
+
+st.write("alr lets see what happens")
+hashtags = ""
+for i in range(10):
+  hashtags += "#"
+  st.write(f"{hashtags} alr lets see what happens: {i + 1} headings")
